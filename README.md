236906540ec9f4263f673fbe3a73f48136a92788
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2023-06-26 12:12:34 +0200

    update

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # goit-react-nodejs-hw-01
+https://prnt.sc/EyJX9zc_9MND
+https://prnt.sc/gz_4Y3H-9Aqx
+https://prnt.sc/fnAzXNvJlngq
--- contacts.js
+++ contacts.js
@@ -1,6 +1,6 @@
 const fs = require("fs").promises;
 const path = require("path");
-require("colors");
+
 
 const contactsPath = path.resolve("./db/contacts.json");
 
